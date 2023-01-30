eed4836a1abbf2cfc7dd45ffc4756b1f71fb3057
zy <953725892@qq.com>
2023-01-31 01:23:47 +0800

    完成对relation微服务中relationAction的基本测试

---
+++ cmd/relation/client/main.go
@@ -0,0 +1,34 @@
+/*
+ * @Author: zy 953725892@qq.com
+ * @Date: 2023-01-31 01:04:01
+ * @LastEditors: zy 953725892@qq.com
+ * @LastEditTime: 2023-01-31 01:11:11
+ * @FilePath: /ByteCamp/cmd/relation/client/main.go
+ * @Description: 用于测试relation微服务
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+package main
+
+import (
+	"context"
+	"douyin/kitex_gen/relation"
+	"douyin/kitex_gen/relation/relationsrv"
+	"fmt"
+
+	"github.com/cloudwego/kitex/client"
+)
+
+func main() {
+	c, _ := relationsrv.NewClient("relation", client.WithHostPorts("127.0.0.1:8888"))
+	request := relation.DouyinRelationActionRequest{
+		UserId:     1,
+		ToUserId:   2,
+		ActionType: 1,
+	}
+	resp, err := c.RelationAction(context.Background(), &request)
+	if err != nil {
+		fmt.Printf("err: %v", err)
+	}
+	fmt.Printf("resp: %v", resp)
+}
