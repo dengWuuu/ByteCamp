0aa240daf6f44e069f5a90ffcd344db93dc97f0a
zy <953725892@qq.com>
2023-01-31 01:03:03 +0800

    完成relation微服务中relationAction接口的代码流程编写

--- cmd/relation/handler.go
+++ cmd/relation/handler.go
@@ -1,26 +1,47 @@
+/*
+ * @Author: zy 953725892@qq.com
+ * @Date: 2023-01-29 21:58:00
+ * @LastEditors: zy 953725892@qq.com
+ * @LastEditTime: 2023-01-31 00:58:15
+ * @FilePath: /ByteCamp/cmd/relation/handler.go
+ * @Description: relation微服务handler
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 package main
 
 import (
 	"context"
+	"douyin/cmd/relation/service"
+	"douyin/dal/pack"
 	relation "douyin/kitex_gen/relation"
 )
 
 // RelationSrvImpl implements the last service interface defined in the IDL.
 type RelationSrvImpl struct{}
 
-// RelationAction implements the RelationSrvImpl interface.
+var (
+	relationService = service.NewRelationService(context.Background())
+)
+
+// 登录用户对其他用户进行关注或取消关注。
 func (s *RelationSrvImpl) RelationAction(ctx context.Context, req *relation.DouyinRelationActionRequest) (resp *relation.DouyinRelationActionResponse, err error) {
 	// TODO: Your code here...
-	return
+	//1、鉴权
+	//2、入参校验
+	//3、调用service层，完成关注或取消关注
+	err = relationService.RelationAction(req)
+	//4、返回结果
+	return pack.BuildRelationActionResponse(err), err
 }
 
-// RelationFollowList implements the RelationSrvImpl interface.
+// 登录用户关注的所有用户列表。
 func (s *RelationSrvImpl) RelationFollowList(ctx context.Context, req *relation.DouyinRelationFollowListRequest) (resp *relation.DouyinRelationFollowListResponse, err error) {
 	// TODO: Your code here...
 	return
 }
 
-// RelationFollowerList implements the RelationSrvImpl interface.
+// 所有关注登录用户的粉丝列表。
 func (s *RelationSrvImpl) RelationFollowerList(ctx context.Context, req *relation.DouyinRelationFollowerListRequest) (resp *relation.DouyinRelationFollowerListResponse, err error) {
 	// TODO: Your code here...
 	return
--- dal/db/follow.go
+++ dal/db/follow.go
@@ -2,7 +2,7 @@
  * @Author: zy 953725892@qq.com
  * @Date: 2023-01-19 11:42:43
  * @LastEditors: zy 953725892@qq.com
- * @LastEditTime: 2023-01-19 14:22:34
+ * @LastEditTime: 2023-01-30 23:55:00
  * @FilePath: /ByteCamp/dal/db/follow.go
  * @Description: 关注实体类及相关crud
  *
@@ -11,7 +11,11 @@
 
 package db
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type Follow struct {
 	gorm.Model
@@ -19,4 +23,32 @@ type Follow struct {
 	FollowId int
 	Version  int
 	Cancel   bool
+}
+
+//添加关注关系
+func AddRelation(userId, followId int) error {
+	follow := Follow{
+		UserId:   userId,
+		FollowId: followId,
+		Version:  0,
+		Cancel:   false,
+	}
+	err := DB.Create(&follow).Error
+	return err
+}
+
+//取消关注关系
+func DeleteRelation(userId, followId int) error {
+	//查询是否存在关注关系
+	var follow Follow
+	result := DB.Where("user_id = ? and follow_id = ?", userId, followId).First(&follow)
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("关注关系不存在")
+	}
+	//更新关注关系
+	err := DB.Model(&follow).Update("cancel", true).Error
+	return err
 }
