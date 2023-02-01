73ed3c0f1d8ef4152d31012eb4b334e999a729c5
202030481266 <1258493030@qq.com>
2023-02-01 18:45:21 +0800

    add rpc server main

--- cmd/comment/main.go
+++ cmd/comment/main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"douyin/dal"
 	comment "douyin/kitex_gen/comment/commentsrv"
 	"log"
+	"net"
+	"os"
+
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/limit"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	kitexzap "github.com/kitex-contrib/obs-opentelemetry/logging/zap"
+	"github.com/spf13/viper"
 )
 
+// Init Relation RPC Server 端配置初始化
+func Init() {
+	dal.Init()
+}
 func main() {
-	svr := comment.NewServer(new(CommentSrvImpl))
-
+	Init()
+	//读取配置
+	path, err1 := os.Getwd()
+	if err1 != nil {
+		panic(err1)
+	}
+	viper.SetConfigName("commentService")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(path + "/config")
+	errV := viper.ReadInConfig()
+	if errV != nil {
+		hlog.Fatal("启动rpc comment服务器时读取配置文件失败")
+		return
+	}
+	klog.SetLogger(kitexzap.NewLogger())
+	klog.SetLevel(klog.LevelDebug)
+	addr, _ := net.ResolveTCPAddr("tcp", viper.GetString("Server.Address")+":"+viper.GetString("Server.Port"))
+	svr := comment.NewServer(new(CommentSrvImpl),
+		server.WithServiceAddr(addr),
+		server.WithLimit(&limit.Option{MaxConnections: 1000, MaxQPS: 100}),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: viper.GetString("Server.Name")}))
 	err := svr.Run()
-
 	if err != nil {
 		log.Println(err.Error())
 	}
--- cmd/relation/main.go
+++ cmd/relation/main.go
@@ -1,15 +1,60 @@
+/*
+ * @Author: zy 953725892@qq.com
+ * @Date: 2023-01-29 21:58:00
+ * @LastEditors: zy 953725892@qq.com
+ * @LastEditTime: 2023-02-01 14:02:20
+ * @FilePath: /ByteCamp/cmd/relation/main.go
+ * @Description: relation rpc server 启动入口
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 package main
 
 import (
+	"douyin/dal"
 	relation "douyin/kitex_gen/relation/relationsrv"
 	"log"
+	"net"
+	"os"
+
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/limit"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	kitexzap "github.com/kitex-contrib/obs-opentelemetry/logging/zap"
+	"github.com/spf13/viper"
 )
 
+// Init Relation RPC Server 端配置初始化
+func Init() {
+	dal.Init()
+}
 func main() {
-	svr := relation.NewServer(new(RelationSrvImpl))
+	Init()
+	//读取配置
+	path, err1 := os.Getwd()
+	if err1 != nil {
+		panic(err1)
+	}
+	viper.SetConfigName("relationService")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(path + "/config")
+	errV := viper.ReadInConfig()
+	if errV != nil {
+		hlog.Fatal("启动rpc relation 服务器时读取配置文件失败")
+		return
+	}
+
+	klog.SetLogger(kitexzap.NewLogger())
+	klog.SetLevel(klog.LevelDebug)
+	addr, _ := net.ResolveTCPAddr("tcp", viper.GetString("Server.Address")+":"+viper.GetString("Server.Port"))
+	svr := relation.NewServer(new(RelationSrvImpl),
+		server.WithServiceAddr(addr),
+		server.WithLimit(&limit.Option{MaxConnections: 1000, MaxQPS: 100}),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: viper.GetString("Server.Name")}))
 
 	err := svr.Run()
-
 	if err != nil {
 		log.Println(err.Error())
 	}
