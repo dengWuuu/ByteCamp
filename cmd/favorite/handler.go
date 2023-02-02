b535e8fc4e7d176c13d5775c664f5540946a49c4
202030481266 <1258493030@qq.com>
2023-02-02 11:47:01 +0800

    add rpc favorite server

--- cmd/favorite/handler.go
+++ cmd/favorite/handler.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"douyin/cmd/favorite/pack"
+	"douyin/cmd/favorite/service"
 	favorite "douyin/kitex_gen/favorite"
+	"douyin/pkg/errno"
 )
 
 // FavoriteSrvImpl implements the last service interface defined in the IDL.
@@ -11,11 +14,28 @@ type FavoriteSrvImpl struct{}
 // FavoriteAction implements the FavoriteSrvImpl interface.
 func (s *FavoriteSrvImpl) FavoriteAction(ctx context.Context, req *favorite.DouyinFavoriteActionRequest) (resp *favorite.DouyinFavoriteActionResponse, err error) {
 	// TODO: Your code here...
-	return
+	favoriteSrv := service.NewFavoriteActionService(ctx)
+	// 检查参数
+	if req.UserId <= 0 || req.VideoId <= 0 || (req.ActionType != 1 && req.ActionType != 2) {
+		return pack.BuildFavoriteActionResp(errno.ErrBind), nil
+	}
+	err = favoriteSrv.FavoriteAction(req)
+	return pack.BuildFavoriteActionResp(err), nil
 }
 
 // FavoriteList implements the FavoriteSrvImpl interface.
 func (s *FavoriteSrvImpl) FavoriteList(ctx context.Context, req *favorite.DouyinFavoriteListRequest) (resp *favorite.DouyinFavoriteListResponse, err error) {
 	// TODO: Your code here...
-	return
+	favoriteSrv := service.NewFavoriteListService(ctx)
+	// 检查参数
+	if req.UserId <= 0 {
+		return pack.BuildFavoriteListResp(errno.ErrBind), nil
+	}
+	vids, err := favoriteSrv.FavoriteList(req)
+	if err != nil {
+		return nil, err
+	}
+	pack_res := pack.BuildFavoriteListResp(err)
+	pack_res.VideoList = vids
+	return pack_res, err
 }
--- cmd/favorite/main.go
+++ cmd/favorite/main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
-	favorite "douyin/kitex_gen/favorite/favoritesrv"
+	"douyin/dal"
+	"douyin/kitex_gen/favorite/favoritesrv"
 	"log"
+	"net"
+	"os"
+
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/limit"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	kitexzap "github.com/kitex-contrib/obs-opentelemetry/logging/zap"
+	"github.com/spf13/viper"
 )
 
+// Init Relation RPC Server 端配置初始化
+func Init() {
+	dal.Init()
+}
 func main() {
-	svr := favorite.NewServer(new(FavoriteSrvImpl))
-
+	Init()
+	//读取配置
+	path, err1 := os.Getwd()
+	if err1 != nil {
+		panic(err1)
+	}
+	viper.SetConfigName("favoriteService")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(path + "/config")
+	errV := viper.ReadInConfig()
+	if errV != nil {
+		hlog.Fatal("启动rpc favorite服务器时读取配置文件失败")
+		return
+	}
+	klog.SetLogger(kitexzap.NewLogger())
+	klog.SetLevel(klog.LevelDebug)
+	addr, _ := net.ResolveTCPAddr("tcp", viper.GetString("Server.Address")+":"+viper.GetString("Server.Port"))
+	svr := favoritesrv.NewServer(new(FavoriteSrvImpl),
+		server.WithServiceAddr(addr),
+		server.WithLimit(&limit.Option{MaxConnections: 1000, MaxQPS: 100}),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: viper.GetString("Server.Name")}))
 	err := svr.Run()
-
 	if err != nil {
 		log.Println(err.Error())
 	}
