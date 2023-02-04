fe10ab22a743eba7f918362c1b54bd09b2ab2d53
yangxinyu.715 <yangxinyu.715@bytedance.com>
2023-02-04 14:47:38 +0800

    :building_construction: WIP video

--- cmd/video/handler.go
+++ cmd/video/handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"douyin/cmd/video/service"
 	video "douyin/kitex_gen/video"
 )
 
@@ -10,24 +11,24 @@ type VideoSrvImpl struct{}
 
 // PublishAction implements the VideoSrvImpl interface.
 func (s *VideoSrvImpl) PublishAction(ctx context.Context, req *video.DouyinPublishActionRequest) (resp *video.DouyinPublishActionResponse, err error) {
-	// TODO: Your code here...
+	resp, err = service.PublishAction(ctx, req)
 	return
 }
 
 // PublishList implements the VideoSrvImpl interface.
 func (s *VideoSrvImpl) PublishList(ctx context.Context, req *video.DouyinPublishListRequest) (resp *video.DouyinPublishListResponse, err error) {
-	// TODO: Your code here...
+	resp, err = service.PublishList(ctx, req)
 	return
 }
 
 // GetUserFeed implements the VideoSrvImpl interface.
 func (s *VideoSrvImpl) GetUserFeed(ctx context.Context, req *video.DouyinFeedRequest) (resp *video.DouyinFeedResponse, err error) {
-	// TODO: Your code here...
+	resp, err = service.GetUserFeed(ctx, req)
 	return
 }
 
 // GetVideoById implements the VideoSrvImpl interface.
 func (s *VideoSrvImpl) GetVideoById(ctx context.Context, req *video.VideoIdRequest) (resp *video.Video, err error) {
-	// TODO: Your code here...
+	resp, err = service.GetVideoById(ctx, req)
 	return
 }
