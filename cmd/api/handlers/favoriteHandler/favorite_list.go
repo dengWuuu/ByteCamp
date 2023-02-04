7daff002632e9526f62d97bd22903aa56ea7537a
202030481266 <1258493030@qq.com>
2023-02-04 12:48:56 +0800

    modify favorite api

--- cmd/api/handlers/favoriteHandler/favorite_list.go
+++ cmd/api/handlers/favoriteHandler/favorite_list.go
@@ -7,25 +7,25 @@ import (
 	"douyin/cmd/favorite/pack"
 	"douyin/kitex_gen/favorite"
 	"douyin/pkg/errno"
+	"strconv"
 
 	"github.com/cloudwego/hertz/pkg/app"
-	"github.com/cloudwego/hertz/pkg/common/hlog"
 )
 
 // FavoriteList 传送http请求上下文到rpc客户端，并且获得客户端的响应
 func FavoriteList(ctx context.Context, c *app.RequestContext) {
 	var favoriteListParam handlers.FavoriteListParam
-	err := c.Bind(&favoriteListParam)
-	if err != nil {
-		hlog.Fatal("序列化喜欢获取请求参数失败")
-		panic(err)
-	}
+	token := c.Query("token")
+	user_id := c.Query("user_id")
 	// 检查参数
-	if favoriteListParam.UserId <= 0 {
+	uid, err := strconv.Atoi(user_id)
+	if err != nil {
 		handlers.SendResponse(c, pack.BuildFavoriteListResp(errno.ErrBind))
 		return
 	}
 	// 封装请求到rpc客户端
+	favoriteListParam.UserId = int64(uid)
+	favoriteListParam.Token = token
 	queryParam := &favorite.DouyinFavoriteListRequest{
 		UserId: favoriteListParam.UserId,
 		Token:  favoriteListParam.Token,
