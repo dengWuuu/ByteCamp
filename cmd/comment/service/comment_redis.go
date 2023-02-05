d223253ac1304534ced3cd45f8b1f97ffb47180f
xiao_shulin <1258493030@qq.com>
2023-02-05 20:52:45 +0800

    add comment redis

--- cmd/comment/service/comment_redis.go
+++ cmd/comment/service/comment_redis.go
@@ -2,16 +2,50 @@ package service
 
 import (
 	"context"
+	"douyin/dal/db"
+	"douyin/kitex_gen/comment"
+	"douyin/kitex_gen/user"
+	"encoding/json"
 	"strconv"
 
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/go-redis/redis/v8"
 )
+
+type CommentUserInfo struct {
+	UserId        int64  `json:"user_id"`
+	Name          string `json:"name"`
+	FollowCount   int64  `json:"follow_count"`
+	FollowerCount int64  `json:"follower_count"`
+	IsFollow      bool   `json:"is_follow"`
+}
+type CommentRedisInfo struct {
+	CommentId  int64           `json:"comment_id"`
+	User       CommentUserInfo `json:"user"`
+	Content    string          `json:"content"`
+	CreateDate string          `json:"create_data"`
+}
+
+// 对象的序列化和反序列化方法
+func (u *CommentUserInfo) MarshalBinary() ([]byte, error) {
+	return json.Marshal(u)
+}
+func (u *CommentUserInfo) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, u)
+}
+func (c *CommentRedisInfo) MarshalBinary() ([]byte, error) {
+	return json.Marshal(c)
+}
+func (c *CommentRedisInfo) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, c)
+}
 
 func getIdCacheName(idType string) string {
 	return "id_" + idType
 }
 
-func GetOneId(RedisDb *redis.Client, idType string) (int64, error) {
+// 使用Lua脚本可以获取唯一的自增ID
+func GetOneId(ctx context.Context, RedisDb *redis.Client, idType string) (int64, error) {
 	key := getIdCacheName(idType)
 	luaId := redis.NewScript(`
 		local id_key = KEYS[1]
@@ -26,7 +60,6 @@ func GetOneId(RedisDb *redis.Client, idType string) (int64, error) {
 		redis.call('set',id_key,result)
 		return tostring(result)
 	`)
-	var ctx = context.Background()
 	n, err := luaId.Run(ctx, RedisDb, []string{key}, 2).Result()
 	if err != nil {
 		return -1, err
@@ -39,4 +72,71 @@ func GetOneId(RedisDb *redis.Client, idType string) (int64, error) {
 			return -1, err
 		}
 	}
+}
+
+// 包装redis数据到RPC数据结构
+func RedisPackComment(ctx context.Context, m CommentRedisInfo) (*comment.Comment, error) {
+	comment_user := &user.User{
+		Id:            m.User.UserId,
+		Name:          m.User.Name,
+		FollowCount:   &m.User.FollowCount,
+		FollowerCount: &m.User.FollowerCount,
+		IsFollow:      m.User.IsFollow,
+	}
+	comment := &comment.Comment{
+		Id:         m.CommentId,
+		User:       comment_user,
+		Content:    m.Content,
+		CreateDate: m.CreateDate,
+	}
+	return comment, nil
+}
+
+func RedisPackComments(ctx context.Context, ms []CommentRedisInfo) ([]*comment.Comment, error) {
+	comments := make([]*comment.Comment, 0)
+	for _, m := range ms {
+		n, err := RedisPackComment(ctx, m)
+		if err == nil {
+			comments = append(comments, n)
+		}
+	}
+	return comments, nil
+}
+
+// 添加评论列表到redis里面
+func AddRedisCommentList(ctx context.Context, vid int64, ms []*comment.Comment) error {
+	comment_map := make(map[string]interface{})
+	for _, m := range ms {
+		if m != nil {
+			// 构造缓存的数据结构
+			userRedisInfo := CommentUserInfo{
+				UserId:        m.User.Id,
+				Name:          m.User.Name,
+				FollowCount:   *m.User.FollowCount,
+				FollowerCount: *m.User.FollowerCount,
+				IsFollow:      m.User.IsFollow,
+			}
+			commentRedisInfo := CommentRedisInfo{
+				CommentId:  m.Id,
+				User:       userRedisInfo,
+				Content:    m.Content,
+				CreateDate: m.CreateDate,
+			}
+			cid_string := strconv.Itoa(int(m.Id))
+			cid_binary, err := commentRedisInfo.MarshalBinary()
+			if err != nil {
+				klog.Fatalf("缓存写入redis时数据序列化错误")
+				return err
+			}
+			comment_map[cid_string] = cid_binary
+		}
+	}
+	// 调用redis的API一次性写入
+	vid_string := strconv.Itoa(int(vid))
+	err := db.CommentRedis.HMSet(ctx, vid_string, comment_map).Err()
+	if err != nil {
+		klog.Fatalf("评论列表写入redis失败")
+		return err
+	}
+	return nil
 }
