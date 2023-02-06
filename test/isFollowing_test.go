3820b7fdf8d0090bb944067198fba2b400bb22d4
zy <953725892@qq.com>
2023-02-06 14:25:53 +0800

    测试IsFollowing函数

--- test/isFollowing_test.go
+++ test/isFollowing_test.go
@@ -1,21 +1,31 @@
+/*
+ * @Author: zy 953725892@qq.com
+ * @Date: 2023-02-06 14:12:54
+ * @LastEditors: zy 953725892@qq.com
+ * @LastEditTime: 2023-02-06 14:25:03
+ * @FilePath: \ByteCamp\test\isFollowing_test.go
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 package test
 
 import (
 	"context"
-	"douyin/cmd/relation/service"
 	"douyin/dal/db"
+	"douyin/pkg/redis"
 	"testing"
 )
 
 func TestIsFollowing(t *testing.T) {
 	db.Init("../config")
 	ctx := context.Background()
-	res, _ := service.IsFollowing(ctx, 1, 2)
+	res, _ := redis.IsFollowing(ctx, 1, 2)
 	t.Log(res)
-	res, _ = service.IsFollowing(ctx, 1, 5)
+	res, _ = redis.IsFollowing(ctx, 1, 5)
 	t.Log(res)
-	res, _ = service.IsFollowing(ctx, 1, 10)
+	res, _ = redis.IsFollowing(ctx, 1, 10)
 	t.Log(res)
-	res, _ = service.IsFollowing(ctx, 23, 24)
+	res, _ = redis.IsFollowing(ctx, 23, 24)
 	t.Log(res)
 }
