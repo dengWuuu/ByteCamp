9e64b611f02293ec0cd9a4f04ec4c7e67e43aecb
Jackson_Wu <dengwu.wu@foxmail.com>
2023-02-07 17:22:07 +0800

    feat: login after register

--- cmd/api/handlers/userHandler/register.go
+++ cmd/api/handlers/userHandler/register.go
@@ -9,8 +9,6 @@ import (
 	"douyin/kitex_gen/user"
 	"douyin/pkg/errno"
 	"github.com/cloudwego/hertz/pkg/app"
-	"github.com/cloudwego/hertz/pkg/common/utils"
-	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
 
 // Register 传递注册http请求到rpc服务
@@ -26,7 +24,7 @@ func Register(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 
-	resp, err := rpc.Register(ctx, &user.DouyinUserRegisterRequest{
+	_, err = rpc.Register(ctx, &user.DouyinUserRegisterRequest{
 		Username: registerParam.UserName,
 		Password: registerParam.PassWord,
 	})
@@ -34,10 +32,6 @@ func Register(ctx context.Context, c *app.RequestContext) {
 		handlers.SendResponse(c, pack.BuildUserRegisterResp(errno.ConvertErr(err)))
 		return
 	}
-	c.JSON(consts.StatusOK, utils.H{
-		"status_code": resp.StatusCode,
-		"status_msg":  resp.StatusMsg,
-		"user_id":     resp.UserId,
-		"token":       resp.Token,
-	})
+	//login after register
+	Login(ctx, c)
 }
