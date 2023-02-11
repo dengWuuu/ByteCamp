cb6d22a2c3e05c16f75884527cbf8c6c7d2caf4c
xiao_shulin <1258493030@qq.com>
2023-02-12 01:10:04 +0800

    update code

--- pkg/prometheus/prometheus_tracer.go
+++ pkg/prometheus/prometheus_tracer.go
@@ -6,6 +6,6 @@ import (
 
 var KitexClientTracer = prometheus.NewClientTracer(":9099", "/kitexclient")
 
-// * var KitexServerTracer = prometheus.NewServerTracer(":9098", "/kitexserver") useless
-// * for some reason the server tracer will open new prometheus server instance
+// TODO: 自定义服务指标和Tracer接口
+// TODO: 使用Sentinel实现限流
 // * 每一个微服务都开启一个prometheus server服务来将metric数据暴露到http中
