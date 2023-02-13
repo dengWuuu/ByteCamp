4ddda3672ce4b91601a3e05dd24e1be811d09eff
xiao_shulin <1258493030@qq.com>
2023-02-13 15:08:04 +0800

    add premetheus.yml

--- pkg/prometheus/prometheus_tracer.go
+++ pkg/prometheus/prometheus_tracer.go
@@ -5,7 +5,3 @@ import (
 )
 
 var KitexClientTracer = prometheus.NewClientTracer(":9099", "/kitexclient")
-
-// TODO: 自定义服务指标和Tracer接口
-// TODO: 使用Sentinel实现限流
-// * 每一个微服务都开启一个prometheus server服务来将metric数据暴露到http中
